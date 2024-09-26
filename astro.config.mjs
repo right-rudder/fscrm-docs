83a5b188dfb634bf8f01efa37ef1d8cb43cd6eed
Diego <diegoyon@outlook.com>
2024-09-26 14:58:34 -0600

    add gtag and facebook pixel

--- astro.config.mjs
+++ astro.config.mjs
@@ -49,6 +49,45 @@ export default defineConfig({
           autogenerate: { directory: "integrations" },
         },
       ],
+      head: [
+        // Facebook Pixel script tag
+        {
+          tag: "script",
+          content: `
+						!function(f,b,e,v,n,t,s)
+						{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+						n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+						if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+						n.queue=[];t=b.createElement(e);t.async=!0;
+						t.src=v;s=b.getElementsByTagName(e)[0];
+						s.parentNode.insertBefore(t,s)}(window, document,'script',
+						'https://connect.facebook.net/en_US/fbevents.js');
+						fbq('init', '270139129390160');
+						fbq('track', 'PageView');
+					`,
+        },
+        // Facebook Pixel noscript tag
+        {
+          tag: "noscript",
+          content: `<img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=270139129390160&ev=PageView&noscript=1" />`,
+        },
+        // Google Analytics script tag
+        {
+          tag: "script",
+          attrs: {
+            async: true,
+            src: "https://www.googletagmanager.com/gtag/js?id=G-TMQCYK3TE3",
+          },
+        },
+        // Google Analytics script tag
+        {
+          tag: "script",
+          content: `window.dataLayer = window.dataLayer || [];
+  					function gtag(){dataLayer.push(arguments);}
+						gtag('js', new Date());
+						gtag('config', 'G-TMQCYK3TE3');`,
+        },
+      ],
     }),
   ],
 });
